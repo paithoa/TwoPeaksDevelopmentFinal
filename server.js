7bb18614c494fa05b417b1cd00dead7283774c62
paithoa <handy.hasan@yahoo.com>
2019-03-05 14:50:39 +1100

    TuesdayMarch 5 commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var axios = require('axios');
 var bodyParser = require("body-parser");
-
+var nodemailer = require('nodemailer');
 var bestSellers=[]
 var task=[]
 var project = [1,2,3]
@@ -15,16 +15,37 @@ var id;
 var newid;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));
+app.use(express.urlencoded())
 app.set('view engine', 'ejs');
 
+function shuffle(arra1) {
+    var ctr = arra1.length, temp, index;
+
+// While there are elements in the array
+    while (ctr > 0) {
+// Pick a random index
+        index = Math.floor(Math.random() * ctr);
+// Decrease ctr by 1
+        ctr--;
+// And swap the last element with it
+        temp = arra1[ctr];
+        arra1[ctr] = arra1[index];
+        arra1[index] = temp;
+    }
+    return arra1;
+}
 
 var port = process.env.PORT || 8080;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
-axios.get('https://newsapi.org/v2/everything?q=realestate&apiKey=5fceaf94ac074d09a5e22cafcbd49b34')
+axios.get('https://newsapi.org/v2/everything?q=realestate&language=en&apiKey=5fceaf94ac074d09a5e22cafcbd49b34')
 .then(response => {
   news = response.data
-  final = news.articles
- console.log(final)
+  final= news.articles
+
+ console.log(shuffle(final))
 
 
 })
@@ -43,6 +64,7 @@ axios.get('https://newsapi.org/v2/top-headlines?country=au&category=business&api
 .catch(error => {
   console.log(error);
 });
+
 
 //takes us to the root(/) URL
 app.get('/', function(req, res){
@@ -94,6 +116,41 @@ app.get('/projects/:id',function(req,res){
 app.get('/contact-us',function(req,res){
   res.render('contact-us')
 });
+
+
+app.post('/submit-form', (req, res) => {
+  const username = req.body.user.name
+  const email = req.body.user.email
+  const subject = req.body.subject
+  const message = req.body.message
+  //...
+  res.end()
+  console.log(username)
+
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'handyhasan301197@gmail.com',
+    pass: 'Paithoa301197'
+  }
+});
+
+var mailOptions = {
+  from: 'handyhasan301197@gmail.com',
+  to: 'derrick.t@twopeaksdevelopment.com',
+  subject: subject,
+  text: "name: " + username + "/n"+ "message: " + message +"/n "+ "email: " + email
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+})
 
 /*
 app.post('/addtask', function (req, res) {
